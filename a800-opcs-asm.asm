9308e6db2fcbf425b09c07decfd1b1dbc78a3703
Greg Ercolano <erco@seriss.com>
2022-01-09 00:07:50 -0800

    Small doc mods, CPU SYNC/ACK on pinout

--- a800-opcs-asm.asm
+++ a800-opcs-asm.asm
@@ -14,33 +14,36 @@ MAXCHANS    equ .4      ; total channels (cpu1=ABCD, cpu2=EFGH)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
-;;                         CPU1                               #                     CPU2
+;; PIC CHIP PIN ASSIGNMENTS
+;; ------------------------
+;;                         CPU1                               #                        CPU2
 ;;  PIN#                  _    _                         PIN# # PIN#               _    _                       PIN#
-;;   1    RES (VPP) MCLR | |__| | RB7 ->(ICSPDAT) D-Dir   28  #  1      " "  MCLR | |__| | RB7 (ICSPDAT) H-Dir   28
-;;   2   STB_8255-> RA0  |      | RB6 ->(ICSPCLK) D-Step  27  #  2      " "  RA0  |      | RB6 (ICSPCLK) H-Step  27
-;;   3   <-ACK_8255 RA1  |      | RB5 ->C-Dir             26  #  3      " "  RA1  |      | RB5 G-Dir             26
-;;   4  SVEL_8255-> RA2  |      | RB4 ->C-Step            25  #  4      " "  RA2  |      | RB4 G-Step            25
-;;   5   CPUID_+5-> RA3  |      | RB3 ->B-Dir             24  #  5 CPUID_GND RA3  |      | RB3 F-Dir             24
-;;   6  <-IRQ_IBMPC RA4  |      | RB2 ->B-Step            23  #  6      " "  RA4  |      | RB2 F-Step            23
-;;   7  <-CPU1 SYNC RA5  |      | RB1 ->A-Dir             22  #  7      " "  RA5  |      | RB1 E-Dir             22
-;;   8        (VSS) GND  |      | RB0 ->A-Step            21  #  8      " "  GND  |      | RB0 E-Step            21
-;;   9  16MHz Clk-> RA7  |      | +5  (VDD)               20  #  9      " "  RA7  |      | +5     "    "         20
-;;   10  CPU1 Ack-> RA6  |      | GND (VSS)               19  #  10     " "  RA6  |      | GND    "    "         19
-;;   11     Data0-> RC0  |      | RC7 <-Data7             18  #  11     " "  RC0  |      | RC7    "    "         18
-;;   12     Data1-> RC1  |      | RC6 <-Data6             17  #  12     " "  RC1  |      | RC6    "    "         17
-;;   13     Data2-> RC2  |      | RC5 <-Data5             16  #  13     " "  RC2  |      | RC5    "    "         16
-;;   14     Data3-> RC3  |______| RC4 <-Data4             15  #  14     " "  RC3  |______| RC4    "    "         15
+;;   1    RES (VPP) MCLR | |__| | RB7 ->(ICSPDAT) D-Dir   28  #  1         " "  MCLR | |__| | RB7 (ICSPDAT) H-Dir   28
+;;   2   STB_8255-> RA0  |      | RB6 ->(ICSPCLK) D-Step  27  #  2         " "  RA0  |      | RB6 (ICSPCLK) H-Step  27
+;;   3   <-ACK_8255 RA1  |      | RB5 ->C-Dir             26  #  3         " "  RA1  |      | RB5 G-Dir             26
+;;   4  SVEL_8255-> RA2  |      | RB4 ->C-Step            25  #  4         " "  RA2  |      | RB4 G-Step            25
+;;   5   CPUID_+5-> RA3  |      | RB3 ->B-Dir             24  #  5    CPUID_GND RA3  |      | RB3 F-Dir             24
+;;   6  <-IRQ_IBMPC RA4  |      | RB2 ->B-Step            23  #  6         " "  RA4  |      | RB2 F-Step            23
+;;   7  <-CPU1 SYNC RA5  |      | RB1 ->A-Dir             22  #  7  CPU1 SYNC-> RA5  |      | RB1 E-Dir             22
+;;   8        (VSS) GND  |      | RB0 ->A-Step            21  #  8         " "  GND  |      | RB0 E-Step            21
+;;   9  16MHz Clk-> RA7  |      | +5  (VDD)               20  #  9         " "  RA7  |      | +5     "    "         20
+;;   10  CPU1 Ack-> RA6  |      | GND (VSS)               19  #  10  <-CPU1 ACK RA6  |      | GND    "    "         19
+;;   11     Data0-> RC0  |      | RC7 <-Data7             18  #  11        " "  RC0  |      | RC7    "    "         18
+;;   12     Data1-> RC1  |      | RC6 <-Data6             17  #  12        " "  RC1  |      | RC6    "    "         17
+;;   13     Data2-> RC2  |      | RC5 <-Data5             16  #  13        " "  RC2  |      | RC5    "    "         16
+;;   14     Data3-> RC3  |______| RC4 <-Data4             15  #  14        " "  RC3  |______| RC4    "    "         15
 ;;                                                            #
-;;                      PIC18F24Q10                           #                 PIC18F24Q10
+;;                      PIC18F24Q10                           #                    PIC18F24Q10
 ;;
-;; NOTE: In "REV-0", RA3 was unused (labeled SMOT).
-;;       In "REV-A", RA3 is now CPU_ID, where the input value is:
+;; NOTE: In "REV-0", RA3 was unused (labeled "SMOT").
+;;       In "REV-A" (and up), RA3 is now the CPU_ID, where the input value is:
 ;;         Logic 1 (+5V) if CPU #1
-;;	   Logic 0 (GND) if CPU #2
+;;	       Logic 0 (GND) if CPU #2
 ;;       This bit is used by CpuSync() to autodetect which CPU we're running on.
 ;;       Previously (in REV-0), we had to build separate binaries for CPU1+2.
 ;;
 ;; SIGNAL MAPPING BETWEEN PC <-> 8255 <-> PIC
+;; ------------------------------------------
 ;;
 ;;     IBMPC        --8255--       --PIC---
 ;;     PORT:MASK    PORT:BIT       PORT:BIT(CPU) SIGNAL NAME
@@ -58,7 +61,9 @@ MAXCHANS    equ .4      ; total channels (cpu1=ABCD, cpu2=EFGH)
 ;;      0302:40  -> PORTC:6    ->  RA2  (CPU1)   SVEL CPU1
 ;;      0302:80  -> PORTC:7    ->  unused        -
 ;;
-;; 8255
+;; 8255 PIN ASSIGNMENTS
+;; --------------------
+;;
 ;;     --8255--
 ;;     PORT:BIT   I/O  DESCRIPTION
 ;;     ========== ==== ===============================
@@ -77,7 +82,9 @@ MAXCHANS    equ .4      ; total channels (cpu1=ABCD, cpu2=EFGH)
 ;;     PORT C:2   OUT  CPU1 START VEL
 ;;     PORT C:3   OUT  unused
 ;;
-;; PROGRAMMER (PICKIT 4)
+;; PICKIT 4 PROGRAMMER 5-PIN CONNECTOR
+;; -----------------------------------
+;;
 ;;     PICKIT PIC  PIC
 ;;     PIN#   PIN# SIGNAL
 ;;     ------ ---- ------
@@ -87,6 +94,7 @@ MAXCHANS    equ .4      ; total channels (cpu1=ABCD, cpu2=EFGH)
 ;;     4      28   ICSP_DAT
 ;;     5      27   ICSP_CLK
 ;;     Do not connect other pins past 5
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; PIC input "data bus" from IBMPC's 8255
